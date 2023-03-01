283fb2b44b83d393f42e34750fa9c6c408104920
Alysson Marley Dias <callistomain@gmail.com>
2023-03-01 20:18:10 -0300

    feat: /payment - back-end info + initial display

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -2,14 +2,28 @@ import { OptionButton } from '../../../components/Dashboard/payment/OptionButton
 import Typography from '@material-ui/core/Typography';
 import { OptionsPannel } from '../../../components/Dashboard/payment/OptionsPannel';
 import styled from 'styled-components';
+import useEnrollment from '../../../hooks/api/useEnrollment';
+import useTicketTypes from '../../../hooks/api/useTicketTypes';
 
+const title = 'Ingresso e pagamento';
 export default function Payment() {
+  const { enrollment } = useEnrollment();
+  const { ticketTypes } = useTicketTypes();
+
+  if (!enrollment) {
+    return (
+      enrollment !== null && <EnrollMessage>
+        <StyledTypography variant="h4">{title}</StyledTypography>
+        <p>Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso</p>
+      </EnrollMessage>
+    );
+  }
+  
   return (
     <>
-      <StyledTypography variant="h4">Suas Informações</StyledTypography>
+      <StyledTypography variant="h4">{title}</StyledTypography>
       <OptionsPannel title="Primeiro, escolha sua modalidade de ingresso">
-        <OptionButton title="Presencial" subTitle="R$ 250"></OptionButton>
-        <OptionButton title="Online" subTitle="R$ 100"></OptionButton>
+        {ticketTypes?.map((e) => <OptionButton key={e.id} title={e.name} subTitle={e.price}></OptionButton>)}
       </OptionsPannel>
     </>
   );
@@ -19,4 +33,30 @@ export default function Payment() {
 
 const StyledTypography = styled(Typography)`
   margin-bottom: 20px !important;
+`;
+
+const EnrollMessage = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  h4 {
+    align-self: flex-start;
+  }
+  
+  p {
+    display: flex;
+    align-items: center;
+    flex: 1;
+
+    max-width: 388px;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    text-align: center;
+
+    color: #8E8E8E;
+  }
 `;
