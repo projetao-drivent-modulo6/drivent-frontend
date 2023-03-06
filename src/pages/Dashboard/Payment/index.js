77874d1a328ca068fdb77d5d1aeed9888eeeee44
RodrigoCosta <costardgg@gmail.com>
2023-03-06 16:54:39 -0300

    Display Confirmation

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -5,26 +5,29 @@ import styled from 'styled-components';
 import { ConfirmButton } from '../../../components/Dashboard/payment/ConfirmButton';
 import useEnrollment from '../../../hooks/api/useEnrollment';
 import useTicketTypes from '../../../hooks/api/useTicketTypes';
-
+//import confirm from '../../../assets/images/Vector.png';
 const title = 'Ingresso e pagamento';
 export default function Payment() {
   const { enrollment } = useEnrollment();
   const { ticketTypes } = useTicketTypes();
-
   if (!enrollment) {
     return (
-      enrollment !== null && <EnrollMessage>
-        <StyledTypography variant="h4">{title}</StyledTypography>
-        <p>Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso</p>
-      </EnrollMessage>
+      enrollment !== null && (
+        <EnrollMessage>
+          <StyledTypography variant="h4">{title}</StyledTypography>
+          <p>Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso</p>
+        </EnrollMessage>
+      )
     );
   }
-  
+
   return (
     <>
       <StyledTypography variant="h4">{title}</StyledTypography>
       <OptionsPannel title="Primeiro, escolha sua modalidade de ingresso">
-        {ticketTypes?.map((e) => <OptionButton key={e.id} title={e.name} subTitle={e.price}></OptionButton>)}
+        {ticketTypes?.map((e) => (
+          <OptionButton key={e.id} title={e.name} subTitle={e.price}></OptionButton>
+        ))}
       </OptionsPannel>
       <ConfirmButton
         title="Fechado! O total ficou em R$ 600. Agora é só confirmar:"
@@ -32,9 +35,57 @@ export default function Payment() {
       ></ConfirmButton>
     </>
   );
+
+  /* return (
+    <>
+      <StyledTypography variant="h4">{title}</StyledTypography>
+      <OptionsPannel title="Ingresso escolhido"></OptionsPannel>
+      <StyledInfoPagamento> </StyledInfoPagamento>
+      <StyledPagamento> Pagamento</StyledPagamento>
+
+      <StyledConfirmation>
+        <img src={confirm} alt="confirm" />
+        <h3>
+          <strong> Pagamento confirmado!</strong> <br />
+          Prossiga para escolha de hospedagem e atividades
+        </h3>
+      </StyledConfirmation>
+    </>
+  );
+  */
 }
 
 //Styled components
+
+/* const StyledInfoPagamento = styled.div`
+  width: 290px;
+  height: 108px;
+  background: #ffeed2;
+  border-radius: 20px;
+  margin-bottom: 15px;
+`;
+const StyledPagamento = styled.h1`
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 23px;
+  color: #8e8e8e;
+`;
+const StyledConfirmation = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 20px;
+  h3 {
+    margin-left: 15px;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 19px;
+    color: gray;
+  }
+`;*/
 
 const StyledTypography = styled(Typography)`
   margin-bottom: 20px !important;
@@ -49,7 +100,7 @@ const EnrollMessage = styled.div`
   h4 {
     align-self: flex-start;
   }
-  
+
   p {
     display: flex;
     align-items: center;
@@ -62,6 +113,6 @@ const EnrollMessage = styled.div`
     font-size: 20px;
     text-align: center;
 
-    color: #8E8E8E;
+    color: #8e8e8e;
   }
 `;
