bcf425d57d1191942ad8612c47bbdd0d2a4a71c4
Alysson Marley Dias <callistomain@gmail.com>
2023-03-20 16:01:55 -0300

    style: small comment placement

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -66,8 +66,8 @@ export default function Activities() {
       setTicketStatus(response.data);
       setIsLoadingTicket(false);
     } catch (error) {
-      // eslint-disable-next-line
       setIsLoadingTicket(false);
+      // eslint-disable-next-line
       console.log(error);
     }
   }
